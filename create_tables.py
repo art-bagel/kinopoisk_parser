c5d08a9c275c29a9aae0fc22f67693544879edbf
art-bagel <mr.bagel@yandex.ru>
2022-05-17 00:27:57 +0300

    Редактирование файла README.md

--- create_tables.py
+++ create_tables.py
@@ -1,8 +1,12 @@
 from postgres_api import PostgresqlAPI
+import settings as st
 
 
 def create_tables():
-    with PostgresqlAPI('kp', 'aleksandrkostenko', '142322', 'localhost') as db:
+    with PostgresqlAPI(st.DB_NAME,
+                       st.DB_USERNAME,
+                       st.DB_PASSWORD,
+                       st.DB_HOST) as db:
         db.create_table_movies()
         db.create_table_genres()
         db.create_table_countries()
